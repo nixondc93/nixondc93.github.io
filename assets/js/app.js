dbf24487904d3009a65af7b771fd5f023de77f2c
Derry Nixon <nixondc93@gmail.com>
2017-02-06 13:45:53 -0800

    updated site with new styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,19 @@
-
-  //
-	// $('#contact').on('click', function(e) {
-	// 	e.preventDefault();
-	// 	$('html, body').animate({ scrollTop: $($(this).attr('#contactme')).offset().top}, 500, 'linear');
-	// });
+$(document).ready(function(){
+  $('.parallax').parallax();
 
 
-$(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
+  $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
         }
-      }
-    });
+      });
+  });
 });
